061cf01a2f1b3f8826921355ff129488c03570e4
sworc <lightbox@nifty.com>
2022-06-16 15:43:58 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -4,7 +4,7 @@
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-    <title>掲示板</title>
+    <title>掲示板 v14 新規投稿画像アップロード</title>
     <link rel="stylesheet" href="client.css?_=<?= time() ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
@@ -108,7 +108,6 @@ $( function(){
             $("#text").val("");
             $("#id").val("");
 
-
             $("#file").val("");
             $("#upload").prop("disabled", true);
 
@@ -138,29 +137,49 @@ $( function(){
 <body>
 <div id="bbs">
     <h3 class="alert alert-primary"><a href="control.php" style="color:black;">掲示板 ( MySQL )</a></h3>
-    <div id="content">
-    <form method="POST" target="myframe" enctype="multipart/form-data">
-        <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
-        <div>
-            <span class="title_entry">タイトル</span> 
-            <input type="text" name="subject" id="subject" pattern=".*\S+.*" required>
-        </div>
-        <div>
-            <span class="title_entry">名前</span>
-            <input type="text" name="name" id="name" pattern="[ぁ-んァ-ン一-龥 　]+" required>
-        </div>
-        <div>
-            <textarea name="text" id="text"></textarea>
-        </div>
-        <div>
-            <input type="submit" name="send" id="send" value="送信">
-            <input type="file" name="file" id="file" accept=".jpg,.jpeg">
-            <input type="button" id="upload" value="画像アップロード" disabled>
-        </div>
-        <input type="hidden" name="datetime" id="datetime">
+    <div id="content"
+        >
+        <form action=""
+            target="myframe"
+            method="POST"
+            enctype="multipart/form-data">
 
-        <input type="hidden" name="id" id="id">
-    </form>
+            <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
+            <div>
+                <span class="title_entry">
+                    タイトル
+                </span>
+                <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    pattern=".*\S+.*"
+                    required
+                    >
+            </div>
+            <div>
+                <span class="title_entry">
+                    名前
+                </span>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    pattern="[ぁ-んァ-ン一-龥 　]+"
+                    required
+                    >
+            </div>
+            <div>
+                <textarea name="text" id="text"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" id="send" value="送信">
+                <input type="file" name="file" id="file" accept=".jpg,.jpeg">
+                <input type="button" id="upload" value="画像アップロード" disabled>
+            </div>
+            <input type="hidden" name="datetime" id="datetime">
+            <input type="hidden" name="id" id="id">
+        </form>
     </div>
 </div>
 
--- view2.php
+++ view2.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>掲示板</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="client.css?_=<?= time() ?>">
