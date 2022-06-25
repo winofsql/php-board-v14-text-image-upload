665f4574148f67d2eb63bb6a1e98281fbd9d20c5
sworc <lightbox@nifty.com>
2022-06-25 18:31:38 +0900

    Update view.php

--- view.php
+++ view.php
@@ -7,7 +7,7 @@
     <title>掲示板 v14 新規投稿画像アップロード</title>
 
 <?php require_once("iframe-css.php") ?>
-<link rel="stylesheet" href="client.css?_=<?= time() ?>">
+    <link rel="stylesheet" href="client.css?_=<?= time() ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js"></script>
